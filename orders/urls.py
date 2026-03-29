8da32a7c358dab559538325fcd65069e16b591ed
sachin123-spec <vani8257979@gmail.com>
2026-03-29 16:10:19 +0530

    fix deploy error

--- orders/urls.py
+++ orders/urls.py
@@ -4,9 +4,3 @@ from .views import checkout
 urlpatterns = [
     path('checkout/', checkout, name='checkout'),
 ]
-from .views import create_admin
-
-urlpatterns = [
-    path('checkout/', checkout, name='checkout'),
-    path('create-admin/', create_admin),
-]
