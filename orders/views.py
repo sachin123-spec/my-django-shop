4dc9a703d67a68c9719b6655774bac42a875e8c0
sachin123-spec <vani8257979@gmail.com>
2026-03-29 15:53:54 +0530

    remove admin creation code for security

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,4 @@
-from django.contrib.auth.models import User
 
-def create_admin(request):
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
-    return HttpResponse("Admin created")
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import Order
